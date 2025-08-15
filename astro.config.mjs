0646ac280e513da98230275f0e631466a87d11c2
samiam376 <sam@withshepherd.com>
2025-08-15 11:17:06 -0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://samiam376.github.io',
-  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
